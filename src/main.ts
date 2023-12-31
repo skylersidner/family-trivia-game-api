4540939e05e4c86f698bb8a12e5283202c3c8a54
Levi Liester <levi.liester@gmail.com>
2023-12-30 21:11:46 -0600

    Forgot to add back in the JWT
    Also ripped out all the passport items

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import passport from 'passport';
-import session, { SessionOptions } from 'express-session';
-import MongoStore from 'connect-mongo';
 import createMongooseConnection, { databaseUrl } from './database';
-import initializePassport from '../passport-config';
 import AccountRoutes from './accounts/accounts.routes';
 import PublicRoutes from './public/public.routes';
 import ErrorHandler from '../error.handler';
@@ -14,9 +10,13 @@ import { omit } from 'lodash';
 import EventsRoutes from './events/events.routes';
 import { IAccount } from './models/accounts';
 import { Server } from 'socket.io';
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from './utils';
 
 declare global {
+    // eslint-disable-next-line @typescript-eslint/no-namespace
     namespace Express {
+        // eslint-disable-next-line @typescript-eslint/no-empty-interface
         export interface User extends IAccount {}
     }
 }
@@ -26,49 +26,6 @@ const isProduction = process.env.NODE_ENV === 'production';
 try {
     const app = express();
     const port = process.env.PORT || 3003;
-
-    // Need to create a different sentry app
-    // Sentry.init({
-    //     dsn: 'https://922c7d37b38344f99ac77ed174bcbbd8@o4505218028994560.ingest.sentry.io/4505218032992256',
-    //     integrations: [
-    //         // enable HTTP calls tracing
-    //         new Sentry.Integrations.Http({ tracing: true }),
-    //         // enable Express.js middleware tracing
-    //         new Sentry.Integrations.Express({ app }),
-    //         // Automatically instrument Node.js libraries and frameworks
-    //         ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
-    //     ],
-    //
-    //     // Set tracesSampleRate to 1.0 to capture 100%
-    //     // of transactions for performance monitoring.
-    //     // We recommend adjusting this value in production
-    //     tracesSampleRate: 1.0,
-    // });
-
-    // Set up session
-
-    const sessionConfig: SessionOptions = {
-        secret: 'back-to-the-future-4',
-        resave: true,
-        proxy: true,
-        saveUninitialized: false,
-        // @ts-ignore
-        store: MongoStore.create({ mongoUrl: databaseUrl }),
-        cookie: {
-            secure: isProduction,
-            maxAge: 1000 * 60 * 60 * 24 * 7,
-            sameSite: 'none',
-        },
-    };
-    if (!isProduction) {
-        delete sessionConfig.cookie; // serve secure cookies
-    }
-    app.use(session(sessionConfig));
-
-    // Set up passport
-    app.use(passport.initialize());
-    app.use(passport.session());
-    initializePassport(passport);
 
     // RequestHandler creates a separate execution context, so that all
     // transactions/spans/breadcrumbs are isolated across requests
@@ -86,6 +43,26 @@ try {
             origin,
         }),
     );
+
+    // JWT Authentication
+    app.use((request, response, next) => {
+        const authorizationHeader = request.headers['authorization'];
+        const token = authorizationHeader?.split(' ')[1];
+        try {
+            if (token) {
+                const user = jwt.verify(token, JWT_SECRET) as IAccount;
+                if (user) {
+                    request.user = user;
+                }
+            }
+            next();
+        } catch (error) {
+            console.log({ message: 'Error verifying token' });
+            return response.status(401).json({ message: 'Session expired' });
+        }
+    });
+
+
     app.use(express.json());
     app.use((req, res, next) => {
         console.log('Request made to: ', req.url);
@@ -97,8 +74,6 @@ try {
     EventsRoutes(app);
     AuthenticationRoutes(app);
     AccountRoutes(app);
-    // The error handler must be before any other error middleware and after all controllers
-    app.use(Sentry.Handlers.errorHandler());
 
     //Error Handler Must Be Last In Middleware
     app.use(ErrorHandler);
